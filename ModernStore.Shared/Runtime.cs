0a7ac9c8754c7f741ac09fd885500f5bf3fc017f
Jorge Olimpia <jorge.olimpia@kpit.com>
2017-09-19 16:35:10 -0300

    Add project files.

---
+++ ModernStore.Shared/Runtime.cs
@@ -0,0 +1,7 @@
+﻿namespace ModernStore.Shared
+{
+    public static class Runtime
+    {
+        public static string ConnectionString = "Server=LAPTOP-TDN79353\\SQLEXPRESS;Initial Catalog=xsd2db;Integrated Security=True";
+    }
+}
